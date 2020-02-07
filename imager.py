d5685fa16865c700ce3bc65458bc8da116200e38
Chad Purdy <chpurdy@gmail.com>
2020-02-06 21:57:00 -0600

    add comments

--- imager.py
+++ imager.py
@@ -7,6 +7,10 @@ MARGIN_WIDTH = 20
 FONT = ImageFont.truetype('/usr/share/fonts/TTF/Inconsolata-Regular.ttf',FONT_SIZE)
 
 def create_image(text_file, out_file):
+    '''
+    Uses text_file to generate an image file saved as out_file
+    '''
+
     text = []
     with open(text_file) as file:
         for line in file:
@@ -18,14 +22,14 @@ def create_image(text_file, out_file):
         if len(line) > widest:
             widest = len(line)
     
-    width = widest * 9
+    width = widest * 9 # This shouldn't be hard-coded
     height = len(text) * FONT_SIZE + 2*MARGIN_WIDTH
 
     img = Image.new("RGB",(width, height))
     draw = ImageDraw.Draw(img)
 
     
-    for i, line in enumerate(text):
+    for i, line in enumerate(text): # I'm not 100% sure of this spacing
         draw.text((MARGIN_WIDTH,MARGIN_WIDTH+i*FONT_SIZE+2),line,font=FONT,fill=(255,255,255))
 
     img.save(out_file)
